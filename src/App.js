2e44043527314d1bf6c9fdb755236285fa7d8743
Razvan Pavel <razvan.e.pavel@gmail.com>
2018-02-18 15:30:12 +0200

    settings files and structure for global scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import './App.scss'
 
 import Routes from './Routes'
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import './styles/index.scss'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
 registerServiceWorker()
